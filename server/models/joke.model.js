00c05c858c0e96e78f52db274c185c52aee82d11
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-07-15 23:15:04 -0400

    adding some validation

--- server/models/joke.model.js
+++ server/models/joke.model.js
@@ -2,9 +2,16 @@ const mongoos = require('mongoose')
 
 
 const JokeSchema = new mongoos.Schema({
-    title : String,
-    teller : String,
-    funniness: Number
+    setup : {
+        type : String,
+        require: [true, "Setup is reqired"],
+        minlength: [10, "Setup must be at least 10 characters!"]
+    },
+    punchline:  {
+        type : String,
+        require: [true, "Punchline is reqired"],
+        minlength: [3, "Setup must be at least 3 characters!"]
+    }
 }, {timestamps: true})
 
 module.exports = mongoos.model('Joke', JokeSchema)
