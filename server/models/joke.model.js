bedcf6628390f86199d0cdae55e25db2b4d120e9
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-07-20 10:31:16 -0400

    some edits

--- server/models/joke.model.js
+++ server/models/joke.model.js
@@ -4,12 +4,12 @@ const mongoos = require('mongoose')
 const JokeSchema = new mongoos.Schema({
     setup : {
         type : String,
-        require: [true, "Setup is reqired"],
+        required: [true, "Setup is reqired"],
         minlength: [10, "Setup must be at least 10 characters!"]
     },
     punchline:  {
         type : String,
-        require: [true, "Punchline is reqired"],
+        required: [true, "Punchline is reqired"],
         minlength: [3, "Setup must be at least 3 characters!"]
     }
 }, {timestamps: true})
